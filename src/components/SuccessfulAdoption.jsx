d5991d18ffe9f435a31cec31194bc3afdfc02e4d
EngineerYussuf <youssef.hassan.mail@gmail.com>
2024-11-28 18:25:17 +0200

    Changes

--- src/components/SuccessfulAdoption.jsx
+++ src/components/SuccessfulAdoption.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./SuccessfulAdoptions.css";
 
 const SuccessfulAdoption = () => {
   return (
@@ -12,37 +13,65 @@ const SuccessfulAdoption = () => {
         <div className="col-lg-3">
           <div className="card border-0 bg-light">
             <div className="cardbody">
-              <img src="./assets/Pics/adop1.jpg" className="img-fluid" alt="Max" />
+              <img
+                src="/src/assets/Pics/adop1.jpg"
+                className="img-fluid"
+                alt="Max"
+              />
             </div>
           </div>
-          <h6>Max: Adopted by Sarah, a compassionate nurse who loves long walks in the park.</h6>
+          <h6>
+            Max: Adopted by Sarah, a compassionate nurse who loves long walks in
+            the park.
+          </h6>
         </div>
 
         <div className="col-lg-3">
           <div className="card border-0 bg-light">
             <div className="cardbody">
-              <img src="./assets/Pics/adop4.jpg" className="img-fluid" alt="Luna" />
+              <img
+                src="/src/assets/Pics/adop4.jpg"
+                className="img-fluid"
+                alt="Luna"
+              />
             </div>
           </div>
-          <h6>Luna: Fostered by James, a freelance graphic designer who enjoys painting.</h6>
+          <h6>
+            Luna: Fostered by James, a freelance graphic designer who enjoys
+            painting.
+          </h6>
         </div>
 
         <div className="col-lg-3">
           <div className="card border-0 bg-light">
             <div className="cardbody">
-              <img src="./assets/Pics/adop2.jpg" className="img-fluid" alt="Buddy" />
+              <img
+                src="/src/assets/Pics/adop2.jpg"
+                className="img-fluid"
+                alt="Buddy"
+              />
             </div>
           </div>
-          <h6>Buddy: Adopted by Emily, a high school teacher known for her love of animals.</h6>
+          <h6>
+            Buddy: Adopted by Emily, a high school teacher known for her love of
+            animals.
+          </h6>
         </div>
 
         <div className="col-lg-3">
           <div className="card border-0 bg-light">
             <div className="cardbody">
-              <img src="./assets/Pics/adop3.jpg" className="img-fluid" alt="Whiskers" />
+              <img
+                src="/src/assets/Pics/adop3.jpg"
+                className="img-fluid"
+                alt="Whiskers"
+              />
             </div>
           </div>
-          <h6>Whiskers: Fostered by David, a retired firefighter who volunteers at the shelter.</h6>
+          <h6>
+            Whiskers: Fostered by David, a retired firefighter who volunteers at
+            the shelter.
+          </h6>
         </div>
       </div>
       <div className="row">
